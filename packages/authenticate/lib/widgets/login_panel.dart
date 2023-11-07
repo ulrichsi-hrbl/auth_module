f298be5bd6f0814194334f4a95605470cadda4f1
ulrichsinn <uli@addtothenoise.com>
2023-11-07 15:55:21 -0800

    LoginPanel started

--- packages/authenticate/lib/widgets/login_panel.dart
+++ packages/authenticate/lib/widgets/login_panel.dart
@@ -1,24 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPanel extends StatefulWidget {
+class LoginPanel extends ConsumerStatefulWidget {
   const LoginPanel({super.key});
 
   @override
-  State<LoginPanel> createState() => _LoginPanelState();
+  ConsumerState<LoginPanel> createState() => _LoginPanelState();
 }
 
-class _LoginPanelState extends State<LoginPanel> {
+class _LoginPanelState extends ConsumerState<LoginPanel> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  bool _isHidden = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    email.dispose();
+    password.dispose();
+    super.dispose();
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SizedBox(height: 20,),
-        Text('Login To Your Account', style: Theme.of(context).textTheme.headlineLarge),
-        Text('TODO: user name'),
-        Text('TODO: password'),
+        const SizedBox(
+          height: 20,
+        ),
+        Text('Login To Your Account',
+            style: Theme.of(context).textTheme.headlineLarge),
+        SizedBox(
+          width: 350,
+          height: 80,
+          child: TextField(
+            controller: email,
+            decoration: const InputDecoration(
+              labelText: "Email",
+              // icon: Icon(Icons.people), //icon at head of input
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 350,
+          height: 100,
+          child: TextField(
+            controller: password,
+            obscureText: _isHidden,
+            decoration: InputDecoration(
+              // icon: const Icon(Icons.lock),
+              //icon at head of input
+              //prefixIcon: Icon(Icons.people), //you can use prefixIcon property too.
+              labelText: "Password",
+              suffixIcon: IconButton(
+                onPressed: _togglePasswordView,
+                icon: Icon(
+                  _isHidden ? Icons.visibility : Icons.visibility_off,
+                ),
+              ), //icon at tail of input
+            ),
+          ),
+        ),
         Text('TODO: login button'),
-        SizedBox(height: 20,),
+        const SizedBox(
+          height: 20,
+        ),
       ],
     );
   }
