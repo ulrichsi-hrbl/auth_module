5db5dd779c8d9d2c6dfd6abceb491727868ffc65
ulrichsi-hrbl <ulrichsi@herbalife.com>
2023-11-14 11:26:31 -0800

    entry point wrapped for riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
