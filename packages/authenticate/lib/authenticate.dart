846272fb755f9be11acf3786daf7df25722a06ba
ulrichsi-hrbl <ulrichsi@herbalife.com>
2024-02-09 13:59:47 -0800

    unused imports removed

--- packages/authenticate/lib/authenticate.dart
+++ packages/authenticate/lib/authenticate.dart
@@ -1,8 +1,4 @@
-import 'package:authenticate/widgets/create_account_panel.dart';
-import 'package:authenticate/widgets/forgot_password_panel.dart';
-import 'package:authenticate/widgets/language_picker_panel.dart';
 import 'package:authenticate/features/authorization/widgets/login_panel.dart';
-import 'package:authenticate/widgets/terms_privacy_panel.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
